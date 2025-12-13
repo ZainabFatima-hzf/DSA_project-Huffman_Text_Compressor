3f9a391c25da2ed3181b0024a59518fe70f0bad1
Zainab Fatima <zainabfatima.hzf@gmail.com>
2025-12-13 16:01:53 +0500

    updated code according to GUI

---
+++ src/models/HuffmanNode.h
@@ -0,0 +1,14 @@
+#pragma once
+
+struct HuffmanNode {
+    char character;
+    int frequency;
+    HuffmanNode* left;
+    HuffmanNode* right;
+
+    // isLeaf() function declaration
+    bool isLeaf() const;
+    // Update constructor
+    HuffmanNode(char d, int f = 0) : character(d), frequency(f), left(nullptr), right(nullptr) {}
+
+};
